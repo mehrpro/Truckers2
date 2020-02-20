675ec2b772ac8abd47e9cfe7d3a43b513413adb4
SEO <SEO@SEO>
2020-02-21 00:14:10 +0330

    981202-2

--- TruckerApp/FrmLogin.Designer.cs
+++ TruckerApp/FrmLogin.Designer.cs
@@ -30,16 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
-            this.txtUsername = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnLogin = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.txtPassword = new DevExpress.XtraEditors.TextEdit();
-            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.btnLogin = new DevExpress.XtraEditors.SimpleButton();
-            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.txtUsername = new DevExpress.XtraEditors.TextEdit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
             this.groupControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // groupControl1
@@ -60,16 +60,41 @@
             this.groupControl1.TabIndex = 0;
             this.groupControl1.Text = "اتوماسیون پایانه";
             // 
-            // txtUsername
+            // simpleButton2
             // 
-            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtUsername.Location = new System.Drawing.Point(32, 83);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Parmida_ActivePlusMultiKeys = false;
-            this.txtUsername.Properties.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
-            this.txtUsername.Properties.Appearance.Options.UseFont = true;
-            this.txtUsername.Size = new System.Drawing.Size(205, 30);
-            this.txtUsername.TabIndex = 0;
+            this.simpleButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.simpleButton2.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
+            this.simpleButton2.Appearance.Options.UseFont = true;
+            this.simpleButton2.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(65, 177);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(100, 49);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "خروج";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
+            this.btnLogin.Appearance.Options.UseFont = true;
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.Location = new System.Drawing.Point(172, 177);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 49);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "ورود";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
+            this.labelControl2.Location = new System.Drawing.Point(243, 126);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(51, 24);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "رمز عبور";
             // 
             // labelControl1
             // 
@@ -93,40 +118,16 @@
             this.txtPassword.Size = new System.Drawing.Size(205, 30);
             this.txtPassword.TabIndex = 0;
             // 
-            // labelControl2
+            // txtUsername
             // 
-            this.labelControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelControl2.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
-            this.labelControl2.Location = new System.Drawing.Point(243, 126);
-            this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(51, 24);
-            this.labelControl2.TabIndex = 1;
-            this.labelControl2.Text = "رمز عبور";
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnLogin.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
-            this.btnLogin.Appearance.Options.UseFont = true;
-            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
-            this.btnLogin.Location = new System.Drawing.Point(172, 177);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(100, 49);
-            this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "ورود";
-            // 
-            // simpleButton2
-            // 
-            this.simpleButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.simpleButton2.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
-            this.simpleButton2.Appearance.Options.UseFont = true;
-            this.simpleButton2.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.Image")));
-            this.simpleButton2.Location = new System.Drawing.Point(65, 177);
-            this.simpleButton2.Name = "simpleButton2";
-            this.simpleButton2.Size = new System.Drawing.Size(100, 49);
-            this.simpleButton2.TabIndex = 2;
-            this.simpleButton2.Text = "خروج";
-            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Location = new System.Drawing.Point(32, 83);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Parmida_ActivePlusMultiKeys = false;
+            this.txtUsername.Properties.Appearance.Font = new System.Drawing.Font("B Yekan", 12F);
+            this.txtUsername.Properties.Appearance.Options.UseFont = true;
+            this.txtUsername.Size = new System.Drawing.Size(205, 30);
+            this.txtUsername.TabIndex = 0;
             // 
             // FrmLogin
             // 
@@ -142,8 +143,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
             this.groupControl1.ResumeLayout(false);
             this.groupControl1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
--- TruckerApp/FrmLogin.cs
+++ TruckerApp/FrmLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraEditors;
 
 namespace TruckerApp
 {
@@ -20,6 +21,41 @@ namespace TruckerApp
         private void simpleButton2_Click(object sender, EventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private byte _UserID { get; set; }
+        private bool _Accsept { get; set; }
+        private string _OpName { get; set; }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string _User, _Pass;
+            _User = txtUsername.Text.Trim();
+            _Pass = txtPassword.Text.Trim();
+            using (var db = new TruckersEntities())
+            {
+                var qry = db.Users.SingleOrDefault(x => x.username == _User);
+                if (qry != null)
+                {
+                    if (qry.password == _Pass)
+                    {
+                        _Accsept = true;
+                        _UserID = qry.userID;
+                        _OpName = $"{qry.FirstName} {qry.FirstName}";
+                    }
+                    else
+                    {
+                        XtraMessageBox.Show("'رمز عبور اشتباه است مجدد تلاش کنید", "اتوماسیون پایانه");
+
+                    }
+
+                }
+                else
+                {
+                    XtraMessageBox.Show("'نام کاربری اشتباه است مجدد تلاش کنید", "اتوماسیون پایانه");
+
+                }
+            }
         }
     }
 }
